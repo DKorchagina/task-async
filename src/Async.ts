71a4ab4947b0a8fd5b834efac2f3e699f8ef83bf
DKorchagina <dasha1972001@yandex.ru>
2022-11-08 16:44:09 +0300

    Задание по асинхронности

--- src/Async.ts
+++ src/Async.ts
@@ -1,8 +1,12 @@
+import { resolve } from "dns";
+import { ModuleResolutionKind } from "typescript";
+
 /*
 Создайте функцию mock, которая принимает на вход аргумент number (количество миллисекунд) и возвращает Promise,
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise(resolve => setTimeout(()=>resolve(ms), ms));
 }
 
 /*
@@ -10,8 +14,18 @@ export function mock(ms: number): Promise<number> {
  */
 export function getData(): Promise<number[]> {
     const result: number[] = [];
-
-    return mock(100)
+    /*const prom1:Promise<number> = new Promise(resolve=>mock(300));
+    const prom2:Promise<number>  = new Promise(resolve=>mock(200));
+    const prom3:Promise<number>  = new Promise(resolve=>mock(100));
+    return Promise.all([prom1, prom2, prom3]).then(values=>{
+        const [data1, data2, data3] = values;
+        result.push(data1); result.push(data2); result.push(data3); 
+        return result;});*/
+    return Promise.all([mock(100), mock(200), mock(300)]).then(values=>{
+        const [data1, data2, data3] = values;
+        result.push(data1); result.push(data2); result.push(data3); 
+        return result;});
+   /* return mock(100)
         .then((data1) => {
             result.push(data1);
             return mock(200);
@@ -23,7 +37,7 @@ export function getData(): Promise<number[]> {
         .then((data3) => {
             result.push(data3);
             return result;
-        });
+        });*/
 }
 
 /*
@@ -31,9 +45,5 @@ export function getData(): Promise<number[]> {
 завершенный с ошибкой Promise и возвращал текст ошибки.
  */
 export async function catchException(): Promise<string | undefined> {
-    try {
-        Promise.reject(new Error('my error'));
-    } catch (err) {
-        return err.message;
-    }
+    return Promise.reject(new Error('my error')).catch((err) => {return err.message});
 }
